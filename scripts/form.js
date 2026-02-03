efb0efe2249e0f54d3d1937191bb998ab98e33a3
Rodrigo Maturano Zanetti <rodrigogitzanetti@outlook.com>
2026-02-03 07:49:01 -0300

    feat: add event listener input on name

--- scripts/form.js
+++ scripts/form.js
@@ -1,8 +1,33 @@
-const formulario = document.querySelector("#form");
-const sendButton = formulario.querySelector(".formulario__form_button");
+/////PEGANDO OS ELEMENTOS PARA O DOM
 
-formulario.addEventListener("submit", function (evt) {
-  evt.preventDefault();
-  console.log("submit capturado");
-  const userName = formulario.querySelector(".classeaqui");
+const { createElement } = require("react");
+
+const formInput = document.querySelector(".formulario__form");
+const inputName = formInput.querySelector("#name");
+const inputDate = formInput.querySelector("#date");
+const inputEmail = formInput.querySelector("#email");
+const inputMessage = formInput.querySelector("#message");
+const inputArquivo = formInput.querySelector("#arquivo");
+
+/////EVITANDO AÇÃO NATIVA DO NAVEGADOR
+
+formInput.addEventListener("submit", (event) => {
+  event.preventDefault();
+});
+
+/////ADICIONANDO FRASE DURANTE O INPUT NAME
+
+let novoElementoMensagem = document.createElement("span");
+inputName.after(novoElementoMensagem);
+
+inputName.addEventListener("input", () => {
+  const validacaoNome = inputName.checkValidity();
+
+  if (!validacaoNome) {
+    novoElementoMensagem.textContent = inputName.validationMessage;
+    novoElementoMensagem.classList.add("span-class");
+  } else {
+    novoElementoMensagem.textContent = "";
+    novoElementoMensagem.classList.remove("span-class");
+  }
 });
