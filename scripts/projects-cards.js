77a15ed231fd9b87e5b984e61d55189916b3ae69
Rodrigo Maturano Zanetti <rodrigogitzanetti@outlook.com>
2026-02-12 22:04:21 -0300

    create like button

--- scripts/projects-cards.js
+++ scripts/projects-cards.js
@@ -1,1 +1,13 @@
-const initialCards = {};
+function createCard() {
+  const templateModel = document
+    .querySelector(".template")
+    .content.querySelector(".card")
+    .cloneNode(true);
+
+  const likeButton = templateModel.querySelector(".card__like-button");
+
+  likeButton.addEventListener("click", () => {
+    likeButton.classList.toggle("like__button-is-on");
+  });
+  return templateModel;
+}
