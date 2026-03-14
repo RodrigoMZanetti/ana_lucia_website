5405bef5c184c8df6846d95c9319030d0ecead84
Rodrigo Maturano Zanetti <rodrigogitzanetti@outlook.com>
2026-03-14 19:48:19 -0300

    new feat: new sidebar on shop html

--- pages/store.js
+++ pages/store.js
@@ -10,3 +10,35 @@ products.forEach((product) => {
   const newCard = new Card(product, storeTemplate);
   storeContainer.append(newCard.generateCard());
 });
+
+////user place
+const buttonOpenUser = document.querySelector(".header__user-button");
+const buttonCloseUser = document.querySelector(".sidebar__close");
+const sidebar = document.querySelector(".sidebar");
+
+buttonOpenUser.addEventListener("click", () => {
+  sidebar.classList.add("sidebar__open");
+});
+
+buttonCloseUser.addEventListener("click", () => {
+  sidebar.classList.remove("sidebar__open");
+});
+
+document.addEventListener("keydown", (evt) => {
+  if (evt.key === "Escape") {
+    sidebar.classList.remove("sidebar__open");
+  }
+});
+
+document.addEventListener("click", (evt) => {
+  const clickedOutsideSidebar = !sidebar.contains(evt.target);
+  const clickedOpenButton = buttonOpenUser.contains(evt.target);
+
+  if (
+    sidebar.classList.contains("sidebar__open") &&
+    clickedOutsideSidebar &&
+    !clickedOpenButton
+  ) {
+    sidebar.classList.remove("sidebar__open");
+  }
+});
